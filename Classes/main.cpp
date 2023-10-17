c4ceb7c625d25427fcaf9f2205dc363d4132fd06
Somya <isomya13@gmail.com>
2023-10-18 03:31:35 +0530

    struct vs classes

---
+++ Classes/main.cpp
@@ -0,0 +1,18 @@
+struct person{
+    int a; //public
+};
+
+class person{
+    int a; //private
+};
+
+
+/*
+structs are passive objects which are used to bundle public data and have little to no functions
+
+classes are active type objects which have members interfaced with functions
+
+classes can be inherited whereas structs can't
+
+null values are possible in class
+*/
